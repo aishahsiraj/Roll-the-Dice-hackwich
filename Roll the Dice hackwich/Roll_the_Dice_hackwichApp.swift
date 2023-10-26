0280f87f4a928c2d01781c500859b8092fad008c
Aishah <aishah122005@gmail.com>
2023-10-26 09:29:44 -0500

    Initial Commit

---
+++ Roll the Dice hackwich/Roll_the_Dice_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roll_the_Dice_hackwichApp.swift
+//  Roll the Dice hackwich
+//
+//  Created by Aishah Siraj on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Roll_the_Dice_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
