43d813a99f2de9b9186bd24a8386ea50acc152b7
Aishah <aishah122005@gmail.com>
2023-10-27 09:50:40 -0500

    Added multiple random values

--- Roll the Dice hackwich/ContentView.swift
+++ Roll the Dice hackwich/ContentView.swift
@@ -9,25 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
             Text("Dice, Roll")
                 .font(.title)
             Image("pips \(randomValue)")
                 .resizable()
-                .frame(width: 200, height:
-                    200, alignment: .center)
+                .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z: 0))
                 .padding()
                 .onTapGesture {
-                    randomValue = Int.random(in: 1...6)
+                    chooseRandom(times: 3)
+                    withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                    }
                 }
             Spacer()
+            
+         }
+     }
+    
+    func chooseRandom(times:Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+ }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
+
